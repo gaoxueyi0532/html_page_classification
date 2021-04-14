a76d920a290425fc0587ffef6b3c88a7152a41dc
Xueyi Gao <37162591+gaoxueyi0532@users.noreply.github.com>
2021-04-14 14:43:08 +0800

    Create ac_automation.py

---
+++ src/ac_automation.py
@@ -0,0 +1,87 @@
+# usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import time, datetime
+import sys
+import os
+import random
+import logging
+from time import sleep
+
+class node(object):
+    def __init__(self):
+        self.next = {}
+        self.fail = None
+        self.isWord = False
+        self.word = ""
+
+class ac_automation(object):
+    def __init__(self):
+        self.root = node()
+
+    def addword(self, word):
+        temp_root = self.root
+        for char in word:
+            if char not in temp_root.next:
+                temp_root.next[char] = node()
+            temp_root = temp_root.next[char]
+        temp_root.isWord = True
+        temp_root.word = word
+
+    def make_fail(self):
+        temp_que = []
+        temp_que.append(self.root)
+        while len(temp_que) != 0:
+            temp = temp_que.pop(0)
+            p = None
+            for key,value in temp.next.item():
+                if temp == self.root:
+                    temp.next[key].fail = self.root
+                else:
+                    p = temp.fail
+                    while p is not None:
+                        if key in p.next:
+                            temp.next[key].fail = p.fail
+                            break
+                        p = p.fail
+                    if p is None:
+                        temp.next[key].fail = self.root
+                temp_que.append(temp.next[key])
+
+    def search(self, content):
+        p = self.root
+        result = []
+        currentposition = 0
+
+        while currentposition < len(content):
+            word = content[currentposition]
+            while word in p.next == False and p != self.root:
+                p = p.fail
+
+            if word in p.next:
+                p = p.next[word]
+            else:
+                p = self.root
+
+            if p.isWord:
+                result.append(p.word)
+                p = self.root
+            currentposition += 1
+        return result
+
+    def parse(self, path):
+        with open(path, 'r') as f:
+            for keyword in f.readlines():
+                self.addword(keyword.strip())
+
+    def hit_key_words(self, text):
+        result = list(set(self.search(text)))
+        if result and len(result) > 0:
+            for res in result:
+                continue
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    ah = ac_automation()
